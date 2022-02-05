fa31fccc278043e642169ad54ce8c649affa8a68
Sarintip <68936860+tipsarin@users.noreply.github.com>
2022-02-06 02:28:54 +0700

    Create cellranger_vdj_bcr.sh

---
+++ cellranger/cellranger_vdj_bcr.sh
@@ -0,0 +1,14 @@
+cd /output-path && \
+
+ref_gex="/path/refdata-cellranger-vdj-GRCh38-alts-ensembl-5.0.0" && \
+fastq_path="/path-of-BCR-fastq-files/" && \
+id_name="idname" && \
+sample_name="prefix-of-BCR-fastq-files" && \
+
+time cellranger vdj --id=$id_name \
+--reference=$ref_vdj \
+--fastqs=$fastq_path \
+--sample=$sample_name \
+--localcores=8 \
+--localmem=64 \
+--chain=IG
