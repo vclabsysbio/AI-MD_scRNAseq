42861deae751df954a535a87e6f8a625b4a9b37b
Sarintip <68936860+tipsarin@users.noreply.github.com>
2022-02-06 02:25:35 +0700

    Update cellranger_count.sh

--- cellranger/cellranger_count.sh
+++ cellranger/cellranger_count.sh
@@ -4,7 +4,7 @@ ref_gex="/path/refdata-gex-GRCh38-2020-A" && \
 fastq_path="/path-of-fastq-files/" && \
 id_name="idname" && \
 sample_name="prefix-of-fastq-file" && \
-expect_cells="22000"
+expect_cells="22000" && \
 
 time cellranger count --id=$id_name \
 --transcriptome=$ref_gex \
